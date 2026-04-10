17b009c70be3109d517b5c3787b43a901528ad09
gewa11281 <desbaina@yahoo.com>
2026-04-09 19:38:59 -0700

    fix main.tf

--- main.tf
+++ main.tf
@@ -24,11 +24,11 @@ module "vpc" {
 }
 
 module "ec2" {
-  source                    = "./ec2"
-  vpc_id                    = module.vpc.vpc_id
-  tags                      = local.project_tags
-  public_subnet_id          = module.vpc.public_subnet_id
-  key_name                  = var.key_name
-  ami_id                    = var.ami_id
-  instance_type             = var.instance_type
+  source           = "./ec2"
+  vpc_id           = module.vpc.vpc_id
+  tags             = local.project_tags
+  public_subnet_id = module.vpc.public_subnet_id
+  key_name         = var.key_name
+  ami_id           = var.ami_id
+  instance_type    = var.instance_type
 }
