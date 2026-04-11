46ca38c91109198eb31cfe98131ea8e26bd0664f
gewa11281 <desbaina@yahoo.com>
2026-04-11 08:30:20 -0700

    fixed app

--- ALB/ALB.tf
+++ ALB/ALB.tf
@@ -28,12 +28,10 @@ resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_jupiter_alb" {
 }
 
 #CREATING OUTBOUND RULES
-resource "aws_vpc_security_group_ingress_rule" "allow_HTTPS_jupiter_alb" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_alb" {
   security_group_id = aws_security_group.jupiter_alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  ip_protocol       = "-1"
 }
 
 # CREATING A TARGET GROUP FOR THE AUTOSCALING GROUP----------------------------------------------
