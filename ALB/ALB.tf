7ad5646380dcf34a4055a0220df43ca0d6c91596
gewa11281 <desbaina@yahoo.com>
2026-04-11 11:39:14 -0700

    added route53-2

--- ALB/ALB.tf
+++ ALB/ALB.tf
@@ -27,6 +27,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_jupiter_alb" {
   to_port           = 80
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_HTTPS_jupiter_alb" {
+  security_group_id = aws_security_group.jupiter_alb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
 #CREATING OUTBOUND RULES
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_alb" {
   security_group_id = aws_security_group.jupiter_alb_sg.id
